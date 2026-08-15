03d6fcbf5dce114177f9e0c67d96a5bad05597d6
tanbinali <tanbinali@gmail.com>
2026-08-15 12:21:11 +0600

    Fixed Email Issue

--- awqat_api/settings.py
+++ awqat_api/settings.py
@@ -216,5 +216,6 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
